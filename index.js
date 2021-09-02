8493a9018e98f3682ee29697dd7759b8b7cbf21a
Muhammad Umar <umar.rajpoot9122@gmail.com>
2021-09-02 10:18:30 -0700

    update

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ app.post('/webhook', (req, res) => {
     if (body.object === 'page') {
   
       body.entry.forEach(function(entry) {
+        console.log(entry);
 
-        // Gets the body of the webhook event
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
+        // // Gets the body of the webhook event
+        // let webhook_event = entry.messaging[0];
+        // console.log(webhook_event);
       
-        // Get the sender PSID
-        let sender_psid = webhook_event.sender.id;
-        console.log('Sender PSID: ' + sender_psid);
+        // // Get the sender PSID
+        // let sender_psid = webhook_event.sender.id;
+        // console.log('Sender PSID: ' + sender_psid);
       
       });
   
