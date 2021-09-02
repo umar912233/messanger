d60c58484f5620fe52c14a9ba8c11c709b116a6c
Muhammad Umar <umar.rajpoot9122@gmail.com>
2021-09-02 09:43:46 -0700

    update

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ app.post('/webhook', (req, res) => {
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
   
-      // Iterates over each entry - there may be multiple if batched
       body.entry.forEach(function(entry) {
-        
-            // Gets the body of the webhook event
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
 
-             // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+      
       });
   
       // Returns a '200 OK' response to all requests
