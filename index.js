4b829f13449609dbdb24b9ef01c8d2570d77694f
umar912233 <60961611+umar912233@users.noreply.github.com>
2021-09-05 07:14:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -97,38 +97,13 @@ function handleMessage(senderPsid, receivedMessage) {
   if (receivedMessage.text) {
     // Create the payload for a basic text message, which
     // will be added to the body of your request to the Send API
+
     response = {
       'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
     };
-  } else if (receivedMessage.attachments) {
 
-    // Get the URL of the message attachment
-    let attachmentUrl = receivedMessage.attachments[0].payload.url;
-    response = {
-      'attachment': {
-        'type': 'template',
-        'payload': {
-          'template_type': 'generic',
-          'elements': [{
-            'title': 'Is this the right picture?',
-            'subtitle': 'Tap a button to answer.',
-            'image_url': attachmentUrl,
-            'buttons': [
-              {
-                'type': 'postback',
-                'title': 'Yes!',
-                'payload': 'yes',
-              },
-              {
-                'type': 'postback',
-                'title': 'No!',
-                'payload': 'no',
-              }
-            ],
-          }]
-        }
-      }
-    };
+    res.send(response);
+
   }
 
   // Send the response message
