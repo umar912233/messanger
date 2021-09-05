e38cca8edd1f49266b37a0490ffecfd663afda08
umar912233 <60961611+umar912233@users.noreply.github.com>
2021-09-05 05:29:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -170,7 +170,6 @@ function callSendAPI(senderPsid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     'uri': 'https://graph.facebook.com/v11.0/me/messages?access_token='+PAGE_ACCESS_TOKEN,
-    'qs': { 'access_token': PAGE_ACCESS_TOKEN },
     'method': 'POST',
     'json': requestBody
   }, (err, _res, _body) => {
