fc98a736d3470d349d8eab7adefbc69fbf7d64c3
umar912233 <60961611+umar912233@users.noreply.github.com>
2021-09-05 05:24:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -158,16 +158,18 @@ function callSendAPI(senderPsid, response) {
   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
   // Construct the message body
-  let requestBody = {
-    'recipient': {
-      'id': senderPsid
-    },
-    'message': response
-  };
+  let requestBody = {"messaging_type": "text",
+"recipient":{
+  "id": senderPsid
+},
+"message":{
+  "text": response
+}
+};
 
   // Send the HTTP request to the Messenger Platform
   request({
-    'uri': 'https://graph.facebook.com/v2.6/me/messages',
+    'uri': 'https://graph.facebook.com/v11.0/me/messages?access_token='+PAGE_ACCESS_TOKEN,
     'qs': { 'access_token': PAGE_ACCESS_TOKEN },
     'method': 'POST',
     'json': requestBody
