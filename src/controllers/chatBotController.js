c8f8bd5edbfd123bd853df72b9b097067909cc77
umar912233 <60961611+umar912233@users.noreply.github.com>
2021-09-08 06:28:12 -0700

    Update chatBotController.js

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -147,7 +147,7 @@ function callSendAPI(sender_psid, response) {
         "json": request_body
     }, (err, res, body) => {
         if (!err) {
-            // console.log('message sent!')
+            console.log('message sent!')
         } else {
             console.error("Unable to send message:" + err);
         }
