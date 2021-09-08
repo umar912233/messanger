68a4923425b92a89144351447da157d5eff8e4a8
umar912233 <60961611+umar912233@users.noreply.github.com>
2021-09-08 03:42:50 -0700

    Update chatBotController.js

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -143,7 +143,7 @@ function callSendAPI(sender_psid, response) {
 
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": "https://graph.facebook.com/v11.0/104174457655406/subscribed_apps?subscribed_fields=feed&access_token="+process.env.FB_PAGE_TOKEN,
+        "uri": "https://graph.facebook.com/v11.0/me/messages",
         "qs": { "access_token": process.env.FB_PAGE_TOKEN },
         "method": "POST",
         "json": request_body
